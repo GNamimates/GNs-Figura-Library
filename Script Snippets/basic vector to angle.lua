75f2cc1106dc09a45297125311732045d4c42090
GNamimates <greenneon188@gmail.com>
2022-04-18 22:44:45 +0800

    cleaned up and added QOL to the function

--- Script Snippets/basic vector to angle.lua
+++ Script Snippets/basic vector to angle.lua
@@ -1,7 +1,7 @@
 -- UP IS Y
-function lookat(a,b)
-    local offset = b-a
-    local y = math.atan2(offset.x,offset.z)
-    local result = vectors.of({math.atan2((math.sin(y)*offset.x)+(math.cos(y)*offset.z),offset.y),y})
+
+function math.toAngle(pos)
+    local y = math.atan2(pos.x,pos.z)
+    local result = vectors.of({math.atan2((math.sin(y)*pos.x)+(math.cos(y)*pos.z),pos.y),y})
     return vectors.of({math.deg(result.x),math.deg(result.y),math.deg(result.z)})
 end

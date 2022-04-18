0fb5b3e33c73787e07ab99f7b1d3b9b1a11c75cd
GNanimates <81744903+GNamimates@users.noreply.github.com>
2022-04-18 18:51:58 +0800

    Update basic vector to angle.lua

--- Script Snippets/basic vector to angle.lua
+++ Script Snippets/basic vector to angle.lua
@@ -1,5 +1,7 @@
 -- UP IS Y
 function lookat(a,b)
+    if type(a) == "table" then a = vectors.of(a) end
+    if type(b) == "table" then b = vectors.of(b) end
     local offset = b-a
     local y = math.atan2(offset.x,offset.z)
     local result = vectors.of({math.atan2((math.sin(y)*offset.x)+(math.cos(y)*offset.z),offset.y),y})

721e85cc3a641e56d84ff954df8cc3ff0c6d601d
MxDuck <aaronduck820@outlook.com>
2023-02-13 12:30:12 -0500

    I fixed it.
    This is outdated.

--- model_files/Shaders/GNs shiny ball/test.js
+++
@@ -1,5 +0,0 @@
-eval
-let fmt = t => (t < 10 ? '0' : '') + t;
-let d = new Date();
-
-`the time is ${fmt(d.getHours()%12)} ${d.getHours() < 12 ? 'AM' : 'PM'} for me`;
--- model_files/Smooth Brain Works/GN migrator/script.lua
+++
@@ -1,8 +0,0 @@
-animation.default.play()
-for _, v in pairs(vanilla_model) do
-    v.setEnabled(false)
-end
-
-function world_render()
-    
-end
